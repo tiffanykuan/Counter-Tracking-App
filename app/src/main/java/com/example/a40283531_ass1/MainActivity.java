938d69a1bb9da065392c853f92e6f40f208ed260
tiffanykuan <t_kua@live.concordia.ca>
2026-01-27 15:45:49 -0500

    mainActivity.java created

--- app/src/main/java/com/example/a40283531_ass1/MainActivity.java
+++ app/src/main/java/com/example/a40283531_ass1/MainActivity.java
@@ -1,24 +1,31 @@
+//Tiffany Kuan
+//40283531
+//COEN 390 - Programming Assignment 1
+
 package com.example.a40283531_ass1;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    //User Interface
+    private Button button1, button2, button3, settingbtn;
+    private TextView totalview;
+    private Button countview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+
+
+
+
+}
+
 }
