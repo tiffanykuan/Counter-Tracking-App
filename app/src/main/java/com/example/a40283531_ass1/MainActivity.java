ae2e672282395a13abecbaedb2c01eddf8c5566a
tiffany <tiffanykuan@Mac.home.local>
2026-01-29 19:54:07 -0500

    id added

--- app/src/main/java/com/example/a40283531_ass1/MainActivity.java
+++ app/src/main/java/com/example/a40283531_ass1/MainActivity.java
@@ -5,23 +5,36 @@
 package com.example.a40283531_ass1;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     //User Interface
-    private Button button1, button2, button3, settingbtn;
+    private Button settingbutton, bevent1, bevent2, bevent3, showcount;
     private TextView totalview;
     private Button countview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
 
 
